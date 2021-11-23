d15f357b9fb54a1c0f96f50684030a5118cd6670
vchepurnov <91080210+vchepurnov@users.noreply.github.com>
2021-11-23 15:05:37 +0300

    Add files via upload

---
+++ Back_v.2/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Back_v._2.IdentityAuth;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Back_v._2.DbContext
+{
+    public class ApplicationDbContext: IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+    }
+}
---
+++ Back_v.2/Models/UserRoles.cs
@@ -0,0 +1,8 @@
+﻿namespace Back_v._2.Models
+{
+    public class UserRoles
+    {
+        public static string Admin = "Admin";
+        public static string User = "User";
+    }
+}
